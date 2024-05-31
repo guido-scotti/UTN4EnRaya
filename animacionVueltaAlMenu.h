371f2c0a5570259d1f3e24906a52a3b9378c65c5
guido-scotti <scottigui@gmail.com>
2024-05-30 23:42:50 -0300

    Create animacionVueltaAlMenu.h

---
+++ animacionVueltaAlMenu.h
@@ -0,0 +1,100 @@
+#ifndef VUELTAALMENUANIMACION_H_INCLUDED
+#define VUELTAALMENUANIMACION_H_INCLUDED
+
+void mostrarCartelVueltaMenu()
+{
+    int i;
+    rlutil::hidecursor();
+    rlutil::setColor(rlutil::WHITE);
+    rlutil::locate(50, 13);
+    cout<<"V";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Vo";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Vol";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volv";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volvi";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volvie";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volvien";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volviend";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volviendo";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volviendo ";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volviendo a";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volviendo al";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volviendo al ";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volviendo al m";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volviendo al me";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volviendo al men";
+    Sleep(150);
+    system("cls");
+    rlutil::locate(50, 13);
+    cout<<"Volviendo al menu";
+    Sleep(150);
+    system("cls");
+
+    for (i=1; i<4; i++) //FOR PARA QUE SE REPITA TRES VECES MAS
+    {
+        rlutil::locate(50, 13);
+        cout<<"Volviendo al menu.";
+        Sleep(150);
+        system("cls");
+        rlutil::locate(50, 13);
+        cout<<"Volviendo al menu..";
+        Sleep(150);
+        system("cls");
+        rlutil::locate(50, 13);
+        cout<<"Volviendo al menu...";
+        Sleep(150);
+        system("cls");
+        rlutil::locate(50, 13);
+        cout<<"Volviendo al menu";
+        Sleep(150);
+        system("cls");
+    }
+}
+
+#endif // VUELTAALMENUANIMACION_H_INCLUDED
+
