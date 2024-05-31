042ec2011d3cbd0ba768c8b21b62b9a353aa8f8b
guido-scotti <scottigui@gmail.com>
2024-05-30 23:32:28 -0300

    cambios para el funcionamiento del juego

--- ingresoJugadores.h
+++ ingresoJugadores.h
@@ -1,23 +1,18 @@
 #ifndef INGRESOJUGADORES_H_INCLUDED
 #define INGRESOJUGADORES_H_INCLUDED
 
-
 char ingresoJugador1(char matrizTablero[][7],int nfilas, int ncol,string jugador1)
 {
 
     int ingresoColumnaUsuario1;
-
     rlutil::locate(31, 1);
     cout<<"- "<<jugador1<<", ingrese la COLUMNA (1 a 7) donde quiera poner su ficha: ";
     cin>>ingresoColumnaUsuario1;
     cout<<endl;
     bool ingresoCorrecto=1;
 
-
     while(ingresoCorrecto)
     {
-        //verificar si el ingreso jugador 1 no es una columna llena
-
         if (ingresoColumnaUsuario1>ncol or ingresoColumnaUsuario1==0)
         {
             rlutil::setColor(rlutil::RED);
@@ -40,21 +35,13 @@ char ingresoJugador1(char matrizTablero[][7],int nfilas, int ncol,string jugador
             if(matrizTablero[5][i] != '-' && ingresoColumnaUsuario1==i+1 )
             {
                 rlutil::setColor(rlutil::RED);
-                rlutil::locate(35, 18);
-
-                cout<<"[Esta columna no existe, ingrese una columna valida]: ";
+                rlutil::locate(34,18);
+                cout<<"[Esta columna esta totalmente ocupada, seleccione otra]: "<<endl;
                 rlutil::setColor(rlutil::WHITE);
-
+                rlutil::locate(91, 18);
                 cin>>ingresoColumnaUsuario1;
-
-
-
-
-                ingresoCorrecto=1;
             }
         }
-
-
 
     }
 
@@ -73,21 +60,18 @@ char ingresoJugador1(char matrizTablero[][7],int nfilas, int ncol,string jugador
 
 }
 
-
-
 char ingresoJugador2(char matrizTablero[][7],int nfilas, int ncol, string jugador2)
 {
-    bool ingresoCorrecto=1;
     int ingresoColumnaUsuario2;
 
     rlutil::locate(31, 1);
     cout<<"- "<<jugador2<<", ingrese la COLUMNA (1 a 7) donde quiera poner su ficha: ";
     cin>>ingresoColumnaUsuario2;
     cout<<endl;
+    bool ingresoCorrecto=1;
 
     while(ingresoCorrecto)
     {
-        //verificar si el ingreso jugador 2 no es una columna llena
         if (ingresoColumnaUsuario2>ncol or ingresoColumnaUsuario2==0)
         {
             rlutil::setColor(rlutil::RED);
@@ -110,22 +94,13 @@ char ingresoJugador2(char matrizTablero[][7],int nfilas, int ncol, string jugado
             if(matrizTablero[5][i] != '-' && ingresoColumnaUsuario2==i+1 )
             {
                 rlutil::setColor(rlutil::RED);
-                rlutil::locate(35, 18);
-
-                cout<<"[Esta columna no existe, ingrese una columna valida]: ";
+                rlutil::locate(34,18);
+                cout<<"[Esta columna esta totalmente ocupada, seleccione otra]: "<<endl;
                 rlutil::setColor(rlutil::WHITE);
-
-                cin>>ingresoColumnaUsuario2 ;
-
-
-
-
-                ingresoCorrecto=1;
+                rlutil::locate(91, 18);
+                cin>>ingresoColumnaUsuario2;
             }
         }
-
-
-
     }
 
     for (int i=0; i<ncol-1; i++)
@@ -140,8 +115,6 @@ char ingresoJugador2(char matrizTablero[][7],int nfilas, int ncol, string jugado
     }
     system("cls");
     mostrarCuadricula(matrizTablero);
-
 }
 
 #endif // INGRESOJUGADORES_H_INCLUDED
-
